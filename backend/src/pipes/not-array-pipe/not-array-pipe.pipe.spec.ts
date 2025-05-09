52f65e2d9e27d159a758a04d693b947194ab88d6
0-kodiya-0 <sanithujayafiverr@gmail.com>
2025-05-09 06:46:29 +0530

    Reconstructing the project

--- backend/src/pipes/not-array-pipe/not-array-pipe.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { NotArrayPipePipe } from './not-array-pipe.pipe';
-
-describe('NotArrayPipePipe', () => {
-  it('should be defined', () => {
-    expect(new NotArrayPipePipe()).toBeDefined();
-  });
-});
